d5dfa013d9983a728eecc24e262d8f75fc5807d2
Simon <simongideon918@gmail.com>
2021-11-24 15:43:15 +0300

    Lesson1

---
+++ Asign 01.py
@@ -0,0 +1,10 @@
+n=int(input("Enter Number: "))
+print(n)
+if n==1:
+	print("Awarded Gold")
+elif n==2:
+	print("Awarded Silver")
+elif n==3:
+	print("Awarded Bronze")
+else:
+	print("Out of Range")
