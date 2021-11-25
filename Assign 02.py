028a223277899b47ac92c5aa935430b66c92545e
Simon <simongideon918@gmail.com>
2021-11-25 20:06:03 +0300

    my daily commit

---
+++ Assign 02.py
@@ -0,0 +1,29 @@
+supermarket = {
+    "Sugar": 110,
+    "Flour": 250,
+    "Rice": 200,
+    "Blue_band": 300
+}
+
+cart = {}
+print("Type exit to finish customer order.....")
+while True:
+    product = input("Enter the product: ")
+    product = product.title()
+    if product in supermarket:
+        quantity = input("Enter Quantity: ")
+        quantity = int(quantity)
+        if type(quantity) == int:
+            cart[product] = quantity
+        else:
+            print("Invalid Quantity")
+    elif product == "Exit":
+        totalPrice = 0
+        for key, value in cart.items():
+            price = supermarket[key] * cart[key]
+            totalPrice += price
+            print(f"{key},{cart[key]} kgs, {supermarket[key]} (perUnit)----> {price}")
+        print(f"Total Price: {totalPrice}")
+        break
+    else:
+        print("Invalid Product........")
